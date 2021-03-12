20f44a27749376211b9c16f24d41cc8cd9342416
Mate2103 <nohab2005@gmail.com>
2021-03-12 11:40:38 +0100

    hard and easyy mode added to guess the color game

--- testing/guessthecolor/script.js
+++ testing/guessthecolor/script.js
@@ -2,7 +2,16 @@ var first = Math.floor(Math.random() * 255) + 1;
 var second = Math.floor(Math.random() * 255) + 1;
 var third = Math.floor(Math.random() * 255) + 1;
 var megoldas;
-function generate(){
+var jatekveg = false;
+var difficulty = true; //false == easy, true == hard
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var idif;
+function generate() {
     first = Math.floor(Math.random() * 255) + 1;
     second = Math.floor(Math.random() * 255) + 1;
     third = Math.floor(Math.random() * 255) + 1;
@@ -12,51 +21,95 @@ function generate(){
     tomb.push(third);
     return tomb;
 }
-function game(){
-    var asd = document.getElementsByTagName('button');
-    for (let i = 0; i < asd.length; i++) {
-        asd[i].style.opacity = 1;
-    }
-    var body =  document.getElementsByTagName('body')[0];
+function game() {
+    jatekveg = false;
+    var body = document.getElementsByTagName('body')[0];
     body.style.backgroundColor = "#2c8e99";
     document.getElementById('correct').innerHTML = "";
     var szinek = generate();
-    var nevek = ['one','two', 'three', 'four', 'five', 'six'];
-    document.getElementById('one').style.backgroundColor = 'rgb(' + [szinek[0],szinek[1],szinek[2]].join(',') + ')';
+    if (difficulty) {
+        var nevek = ['one', 'two', 'three', 'four', 'five', 'six'];
+        for (let i = 0; i < nevek.length; i++) {
+            document.getElementById(nevek[i]).style.opacity = 1;
+        }
+        idif = 6;
+        document.getElementById('hard').style.backgroundColor = "#2c8e99";
+        document.getElementById('hard').style.color = "white";
+        document.getElementById('easy').style.backgroundColor = "white";
+        document.getElementById('easy').style.color = "#2c8e99";
+    } else if (!difficulty) {
+        var no = ['four', 'five', 'six'];
+        var nevek = ['one', 'two', 'three'];
+        for (let i = 0; i < nevek.length; i++) {
+            document.getElementById(nevek[i]).style.opacity = 1;
+            document.getElementById(no[i]).style.opacity = 0;
+        }
+        idif = 3;
+        document.getElementById('easy').style.backgroundColor = "#2c8e99";
+        document.getElementById('easy').style.color = "white";
+        document.getElementById('hard').style.backgroundColor = "white";
+        document.getElementById('hard').style.color = "#2c8e99";
+    }
+    document.getElementById('one').style.backgroundColor = 'rgb(' + [szinek[0], szinek[1], szinek[2]].join(',') + ')';
     var randomhely = Math.floor(Math.random() * 6);
-    for (let i = 1; i < 6; i++) {
+    for (let i = 0; i < idif; i++) {
         if (randomhely == i) {
             szinek = generate();
             var kiirkod = "RGB kód: " + szinek[0] + " " + szinek[1] + " " + szinek[2];
             document.getElementById('kod').innerHTML = kiirkod;
-            document.getElementById(nevek[i]).style.backgroundColor = 'rgb(' + [szinek[0],szinek[1],szinek[2]].join(',') + ')';
+            document.getElementById(nevek[i]).style.backgroundColor = 'rgb(' + [szinek[0], szinek[1], szinek[2]].join(',') + ')';
             megoldas = nevek[i];
         } else {
             szinek = generate();
-            document.getElementById(nevek[i]).style.backgroundColor = 'rgb(' + [szinek[0],szinek[1],szinek[2]].join(',') + ')';
+            document.getElementById(nevek[i]).style.backgroundColor = 'rgb(' + [szinek[0], szinek[1], szinek[2]].join(',') + ')';
         }
     }
-    console.log("asd");
     console.log(megoldas);
 }
 function check(nev) {
-    if (nev==megoldas) {
-        console.log('jessszz');
-        Helyes(nev);
+    if (!jatekveg) {
+        if (nev == megoldas) {
+            Helyes(nev);
+            jatekveg = true;
+        } else {
+            document.getElementById('correct').innerHTML = "Próbáld újra";
+            document.getElementById(nev).style.opacity = 0;
+        }
     } else {
-        console.log('anyád');
-        document.getElementById('correct').innerHTML = "Próbáld újra";
-        document.getElementById(nev).style.opacity = 0;
+        return;
     }
 }
 function Helyes(nev) {
-    var body =  document.getElementsByTagName('body')[0];
+    var body = document.getElementsByTagName('body')[0];
     document.getElementById('correct').innerHTML = "Helyes";
     var megoldasszin = document.getElementById(nev);
     body.style.backgroundColor = megoldasszin.style.backgroundColor;
     var negyzetek = document.querySelectorAll('.square');
-    for (let i = 0; i < negyzetek.length; i++) {
+    for (let i = 0; i < idif; i++) {
         negyzetek[i].style.opacity = 1;
         negyzetek[i].style.backgroundColor = megoldasszin.style.backgroundColor;
     }
+}
+function hardeasy(hardeasy) {
+    var hard = 'hard';
+    var easy = 'easy';
+    var hardid = document.getElementById(hard);
+    var easyid = document.getElementById(easy);
+    if (hardeasy == hard) {
+        difficulty = true;
+        hardid.style.backgroundColor = "#2c8e99";
+        hardid.style.color = "white";
+        easyid.style.backgroundColor = "white";
+        easyid.style.color = "#2c8e99";
+        game();
+    }
+    else if (hardeasy == easy) {
+        difficulty = false;
+        easyid.style.backgroundColor = "#2c8e99";
+        easyid.style.color = "white";
+        hardid.style.backgroundColor = "white";
+        hardid.style.color = "#2c8e99";
+        game();
+    }
+    console.log(difficulty);
 }
